00db515960c807059d1676be983dbe44dd8b347f
matheustkaczyk <matheus111904@hotmail.com>
2022-01-24 16:05:57 -0300

    Estruturaçao model Users

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,12 @@
+const User = (sequelize, DataTypes) => {
+  module.exports = sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false }
+  },
+  { timestamps: false });
+
+  return User;
+};
