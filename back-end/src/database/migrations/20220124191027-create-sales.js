821dc2d1b5034a9efb47cfd093673dcfd845d06e
matheustkaczyk <matheus111904@hotmail.com>
2022-01-24 16:43:36 -0300

    Estruturando migration Sales

--- back-end/src/database/migrations/20220124191027-create-sales.js
+++ back-end/src/database/migrations/20220124191027-create-sales.js
@@ -8,17 +8,43 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      id: {
-        type: Sequelize.INTEGER
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: { model: 'Users', key: 'id' },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: { model: 'Users', key: 'id' },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      total_price: {
+        type: Sequelize.DECIMAL(9, 2),
+        allowNull: false,
+      },
+      delivery_address: {
+        type: Sequelize.STRING(100), 
+        allowNull: false,
+      },
+      delivery_number: {
+        type: Sequelize.STRING(50), 
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.STRING(50),
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'sale_date',
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
     });
   },
   down: async (queryInterface, Sequelize) => {
