51f5b56ff41e3f714d0fb460a502eb7bd96a9d17
Adryan Rosa <adryan.lmrosa@gmail.com>
2022-01-25 18:07:46 -0300

    Cria setup inicial Redux com exemplo

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux'
