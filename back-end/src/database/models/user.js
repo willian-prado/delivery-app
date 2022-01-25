0a96e6be2f02cae4dcb07cbf4f47800d262dbc96
matheustkaczyk <matheus111904@hotmail.com>
2022-01-25 14:16:30 -0300

    Corrigido quantidade de caracteres no model do User

--- back-end/src/database/models/user.js
+++ back-end/src/database/models/user.js
@@ -1,10 +1,10 @@
 const User = (sequelize, DataTypes) => {
   module.exports = sequelize.define("User", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
-    name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.STRING, allowNull: false }
+    name: { type: DataTypes.STRING(255), allowNull: false },
+    email: { type: DataTypes.STRING(255), allowNull: false, unique: true },
+    password: { type: DataTypes.STRING(255), allowNull: false },
+    role: { type: DataTypes.STRING(255), allowNull: false }
   },
   { timestamps: false });
 
