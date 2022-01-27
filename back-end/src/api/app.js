cce539c1dc37e6914516a164b527ea6fe291c9bd
Daniel Batista <danielbped@gmail.com>
2022-01-27 15:25:00 -0300

    Torna imagens publicas

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 app.use('/login', loginRouter);
 app.use('/user', usersRouter);
 
+app.use(express.static('public'));
+
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
